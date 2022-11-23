1725feca3ce7b18da62d6c18979ecdff0ea1da9d
Jesus Bolivar <j5rojo94@gmail.com>
2022-11-23 11:28:50 -0300

    Endpoint /usuarios - Filtros por parametros [GET]

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
+    protected static array $status = [
+        'true' => 'active',
+        'false' => 'inactive'
+    ];
+
+    protected static array $gender = [
+        'hombre' => 'male',
+        'mujer' => 'female'
+    ];
+
     public function all( Request $request, $page = null ) {
 
         $request->merge( [ 'page' => $request->route('page') ] );
@@ -25,16 +36,42 @@ class UsersController extends Controller
 
         $apiUrl = env('GO_REST_API_URL');
 
-        $requestUrl = ( !is_null($page) )
-            ? $apiUrl . '/users?page=' . $page
-            : $apiUrl . '/users';
+        $requestUrl = $apiUrl . '/users';
+        $queryParameters = [];
 
-        $request = Http::get( $requestUrl );
+        if ( !is_null($page) ) {
+            $queryParameters['page'] = $page;
+        }
+
+        if ( $request->has('nombre') ) {
+            $queryParameters['name'] = $request->query('nombre');
+        }
+
+        if ( $request->has('email') ) {
+            $queryParameters['email'] = $request->query('email');
+        }
+
+        if ( $request->has('genero') ) {
+            if ( !in_array( $request->query('genero'), array_flip( self::$gender ) ) ) {
+                return response()->json([
+                    'message' => 'Valor de genero no soportado',
+                ], 400);
+            }
+
+            $queryParameters['gender'] = self::$gender[ $request->query('genero') ];
+        }
+
+        if ( $request->has('activos') ) {
+            $queryParameters['status'] = self::$status[ $request->query('activos') ];
+        }
+
+        $request = Http::get( $requestUrl . '?' . Arr::query( $queryParameters ) );
 
         if ( $request->failed() ) {
             return response()->json([
                 'message' => 'goREST Request error',
-            ]);
+                'details' => $request->json()
+            ], 400);
         }
 
         $response = $request->json();
@@ -45,7 +82,7 @@ class UsersController extends Controller
                 'id' => $user['id'],
                 'nombre' => $user['name'],
                 'email' => $user['email'],
-                'genero' => $user['gender'],
+                'genero' => array_flip( self::$gender )[ $user['gender'] ],
                 'activo' => $user['status'] === 'active',
             ];
         }
@@ -55,5 +92,6 @@ class UsersController extends Controller
         }
 
         return response()->json( $users );
+
     }
 }
